8a45605f313fcab7a6c706623bd429e88a05eb67
soheil <soheilrasekh555@gmail.com>
2022-05-19 14:46:53 +0430

    sort subscription plans

--- packages/frontend/hooks/useSubscriptionPlans.ts
+++ packages/frontend/hooks/useSubscriptionPlans.ts
@@ -1,12 +1,17 @@
 import useSWR from "swr";
 import { Backer } from "../contracts/backer/Backer";
+import { SubscriptionPlan } from "../types/subscription-plan";
 import useBackerContract from "./useBackerContract";
 import useKeepSWRDataLiveAsBlocksArrive from "./useKeepSWRDataLiveAsBlocksArrive";
 
 function getSubscriptionPlans(backer: Backer) {
   return async (_: string, creator: string) => {
     try {
-      return await backer.getCreatorSubscriptionPlans(creator);
+      let plans = await backer.getCreatorSubscriptionPlans(creator);
+      plans = [...plans].sort((a, b) =>
+        a.id.toString().localeCompare(b.id.toString())
+      );
+      return plans as SubscriptionPlan[];
     } catch (error) {
       console.error(error);
     }
