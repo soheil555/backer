f2a8f623e7bcb421eef2837008fbc216b0cb69dd
soheil <soheilrasekh555@gmail.com>
2022-05-21 20:49:16 +0430

    useClipboard hook

--- packages/frontend/components/User/UserInfo.tsx
+++ packages/frontend/components/User/UserInfo.tsx
@@ -1,15 +1,38 @@
-import { Box, Heading, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  Tooltip,
+  useClipboard,
+  useToast,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
 import useAppSelector from "../../hooks/useAppSelector";
 import Card from "../Card/Card";
 
 export default function UserInfo() {
+  const toast = useToast();
   const { address, user } = useAppSelector((state) => state.web3);
+  const { hasCopied: addressHasCopied, onCopy: addressOnCopy } = useClipboard(
+    address ?? ""
+  );
+  const { hasCopied: userHasCopied, onCopy: userOnCopy } = useClipboard(
+    user?.sub ?? ""
+  );
 
   const isUser = !!user;
 
-  const copyToClipboard = (text: string) => {
-    navigator.clipboard.writeText(text);
-  };
+  useEffect(() => {
+    if (userHasCopied || addressHasCopied) {
+      toast({
+        title: "Copied",
+        description: "Copied to clipboard.",
+        status: "info",
+        duration: 1000,
+        isClosable: true,
+      });
+    }
+  }, [userHasCopied, addressHasCopied]);
 
   return (
     <Card alignItems="center" flexWrap="wrap" gap={4}>
@@ -18,13 +41,7 @@ export default function UserInfo() {
           <Heading fontWeight="light" mb={1} fontSize={20}>
             UNS Domain name
           </Heading>
-          <Text
-            onClick={() => {
-              copyToClipboard(user.sub);
-            }}
-            cursor="pointer"
-            display="inline"
-          >
+          <Text onClick={userOnCopy} cursor="pointer" display="inline">
             <Tooltip hasArrow label="copy to clipboard">
               {user.sub}
             </Tooltip>
@@ -36,13 +53,7 @@ export default function UserInfo() {
         <Heading fontWeight="light" mb={1} fontSize={20}>
           Wallet Address
         </Heading>
-        <Text
-          onClick={() => {
-            copyToClipboard(address ?? "");
-          }}
-          display="inline"
-          cursor="pointer"
-        >
+        <Text onClick={addressOnCopy} display="inline" cursor="pointer">
           <Tooltip hasArrow label="copy to clipboard">
             {address ?? ""}
           </Tooltip>
